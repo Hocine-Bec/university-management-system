3690e08ed09a74e5111f1590e9e3b4af95d70c44
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-07-08 19:45:16 +0100

    refactor(Applications): Remove UserRole from User DTOs and Services
    - Removed UserRole property from CreateUserRequest and UpdateUserRequest DTOs to simplify user management.
    - Removed UpdateUserRoleRequest DTO and related service method.  User roles are now managed separately.
    - Added GetByRoleAsync method to IUserRepository and IUserService to retrieve users by role ID.  This allows for more flexible role-based access control.
    - Updated UserRepository to efficiently retrieve users by role using Include and ThenInclude for optimized database queries.

--- Applications/Interfaces/Services/IUserService.cs
+++ Applications/Interfaces/Services/IUserService.cs
@@ -1,18 +1,17 @@
 ﻿using Applications.DTOs.Users;
 using Applications.Shared;
 
-namespace Applications.Interfaces.Services
+namespace Applications.Interfaces.Services;
+
+public interface IUserService
 {
-    public interface IUserService
-    {
-        Task<Result<UserResponse>> AddAsync(CreateUserRequest request);
-        Task<Result> ChangePasswordAsync(int id, ChangePasswordRequest request);
-        Task<Result> DeleteAsync(int id);
-        Task<Result> DeleteAsync(string username);
-        Task<Result<UserResponse>> GetByIdAsync(int id);
-        Task<Result<UserResponse>> GetByUsernameAsync(string username);
-        Task<Result<IReadOnlyCollection<UserResponse>>> GetListAsync();
-        Task<Result> UpdateAsync(int id, UpdateUserRequest request);
-        Task<Result> UpdateUserRoleAsync(int id, UpdateUserRoleRequest roleStatus);
-    }
+    Task<Result<UserResponse>> AddAsync(CreateUserRequest request);
+    Task<Result> ChangePasswordAsync(int id, ChangePasswordRequest request);
+    Task<Result> DeleteAsync(int id);
+    Task<Result> DeleteAsync(string username);
+    Task<Result<UserResponse>> GetByIdAsync(int id);
+    Task<Result<UserResponse>> GetByUsernameAsync(string username);
+    Task<Result<IReadOnlyCollection<UserResponse>>> GetListAsync();
+    Task<Result> UpdateAsync(int id, UpdateUserRequest request);
+    Task<Result<IReadOnlyCollection<UserResponse>>> GetByRoleAsync(int roleId);
 }
