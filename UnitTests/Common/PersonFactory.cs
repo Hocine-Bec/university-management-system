6bb0e081e5089a1de9a3ec78fbaddd7a6d498044
Hocine-Bec <Bechebil.houcin@gmail.com>
2025-07-14 20:02:37 +0100

    feat(tests): add PersonFactory for generating test data
    - Create PersonFactory utility class
    - Implement CreateTestPeople method to generate mock Person entities
    - Utilize Bogus library for realistic fake data generation
    - Support custom seeding for reproducible test data
    - Include country assignment from provided list
    - Generate complete Person objects with all required properties

---
+++ UnitTests/Common/PersonFactory.cs
@@ -0,0 +1,32 @@
+using Bogus;
+using Domain.Entities;
+using Person = Domain.Entities.Person;
+
+namespace UnitTests.Common;
+
+public static class PersonFactory
+{
+    public static List<Person> CreateTestPeople(int count, List<Country> countries, int? seed = null)
+    {
+        var faker = new Faker<Person>()
+            .RuleFor(p => p.Id, (f, p) => f.IndexGlobal + 1)
+            .RuleFor(p => p.FirstName, f => f.Name.FirstName())
+            .RuleFor(p => p.LastName, f => f.Name.LastName())
+            .RuleFor(p => p.DOB, f => f.Date.Past(30, DateTime.Now.AddYears(-18)))
+            .RuleFor(p => p.Address, f => f.Address.StreetAddress())
+            .RuleFor(p => p.PhoneNumber, f => f.Phone.PhoneNumber())
+            .RuleFor(p => p.Email, (f, u) => f.Internet.Email(u.FirstName, u.LastName))
+            .RuleFor(p => p.ImagePath, f => f.Internet.Avatar())
+            .FinishWith((f, p) =>
+            {
+                var country = f.PickRandom(countries);
+                p.CountryId = country.Id;
+                p.Country = country;
+            });
+
+        if (seed.HasValue)
+            faker.UseSeed(seed.Value); 
+
+        return faker.Generate(count);
+    }
+}
